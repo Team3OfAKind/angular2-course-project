f42989ebe0c94ba13c50761390b27b4e62741a45
Elena Zarkova <ezarkova@gmail.com>
2016-12-29 15:35:02 +0200

    Like-dislike button template logic

--- src/app/menu/meal-menu/meal-menu.component.ts
+++ src/app/menu/meal-menu/meal-menu.component.ts
@@ -5,42 +5,59 @@ import { Meal } from '../../../models/meal-model';
 import { User } from '../../../models/user-model';
 
 @Component({
-  selector: 'meal-menu',
-  templateUrl: './meal-menu.component.html',
-  styleUrls: ['./meal-menu.component.css']
+    selector: 'meal-menu',
+    templateUrl: './meal-menu.component.html',
+    styleUrls: ['./meal-menu.component.css']
 })
 export class MealMenuComponent implements OnInit {
-  isInCart: boolean;
-  isFavourite: boolean;
-  options: Object;
+    hasLoggedUser: boolean;
+    isInCart: boolean;
+    isFavourite: boolean;
+    options: Object;
+    likeButtonText: string;
 
-  @Input() meal: Meal;
-  @Input() user: User;
+    @Input() meal: Meal;
+    @Input() user: User;
 
-  constructor(private userService: UserService, private notification: NotificationsService) { }
+    constructor(private userService: UserService, private notification: NotificationsService) { }
 
-  ngOnInit() {
-    this.isInCart = true;
-    this.isFavourite = true;
-    if (this.user) {
-      this.isInCart = this.user.cartMeals.find(m => m._id === this.meal._id);
-      this.isFavourite = false;
+    ngOnInit() {
+        this.hasLoggedUser = false;
+        if (this.user) {
+            this.hasLoggedUser = true;
+            this.isInCart = this.user.cartMeals.find(m => m._id === this.meal._id);
+            this.isFavourite = false;
+            this.likeButtonText = 'Like';
+            //this.isFavourite = this.user.favouriteMeals.find(m => m._id === this.meal._id);
+        }
+        this.options = { timeOut: 2500, pauseOnHover: true, showProgressBar: false, animate: 'scale', position: ['right', 'top'] };
     }
-    this.options = { timeOut: 2500, pauseOnHover: true, showProgressBar: false, animate: 'scale', position: ['right', 'top'] };
-  }
 
-  ngOnChanges(){
-     if (this.user) {
-      this.isInCart = this.user.cartMeals.find(m => m._id === this.meal._id);
-      this.isFavourite = false;
+    ngOnChanges() {
+        if (this.user) {
+            this.hasLoggedUser = true;
+            this.isInCart = this.user.cartMeals.find(m => m._id === this.meal._id);
+            this.isFavourite = false;
+            this.likeButtonText = 'Like';
+            //this.isFavourite = this.user.favouriteMeals.find(m => m._id === this.meal._id);
+        }
     }
-  }
 
-  addToCart() {
-    this.userService.addMealToCart(this.meal)
-      .subscribe((res) => {
-        this.notification.success('', res.result.message);
-        this.isInCart = true;
-      });
-  }
+    addToCart() {
+        this.userService.addMealToCart(this.meal)
+            .subscribe((res) => {
+                this.notification.success('', res.result.message);
+                this.isInCart = true;
+            });
+    }
+
+    changeLike() {
+        if (this.isFavourite) {
+            this.isFavourite = false;
+            this.likeButtonText = 'Like';
+        } else {
+            this.isFavourite = true;
+            this.likeButtonText = 'Dislike';
+        }
+    }
 }
