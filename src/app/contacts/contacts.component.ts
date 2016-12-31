f8e2cf4ad2036087f06a95a894ffde304c88f430
Silviya <sbboteva@gmail.com>
2016-12-31 11:58:29 +0200

    contact page now done

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-contacts',
   templateUrl: './contacts.component.html',
-  styleUrls: ['./contacts.component.css']
+  styleUrls: ['../app.component.css', './contacts.component.css']
 })
 export class ContactsComponent implements OnInit {
 
