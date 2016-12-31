c9314405688a14f49b3a028f635072df0005fe18
Silviya <sbboteva@gmail.com>
2016-12-31 17:22:46 +0200

    profile page now done

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -6,7 +6,7 @@ import {User} from '../../models/user-model';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['../app.component.css', './profile.component.css']
 })
 export class ProfileComponent implements OnInit {
   user: User;
