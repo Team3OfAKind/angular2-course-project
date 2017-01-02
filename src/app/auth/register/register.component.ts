9364c2fab46615244fd7da1c9cefc5176b687ce8
Elena Zarkova <ezarkova@gmail.com>
2017-01-02 18:29:07 +0200

    Some error handling

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -22,28 +22,35 @@ export class RegisterComponent implements OnInit {
 
   ngOnInit() {
     this.userToRegister = this.fb.group({
-      'username': ['', Validators.compose([Validators.required, Validators.minLength(5)])],
-      'firstName': ['', Validators.compose([Validators.required, Validators.minLength(5)])],
-      'lastName': ['', Validators.compose([Validators.required, Validators.minLength(5)])],
-      'email': ['', Validators.required],
-      'password': ['', Validators.compose([Validators.required, Validators.minLength(4)])],
-      'confirmedPassword': ['', Validators.compose([Validators.required, Validators.minLength(4)])]
+      username: ['', Validators.compose([Validators.required, Validators.minLength(5), Validators.maxLength(15)])],
+      firstName: ['', Validators.compose([Validators.required, Validators.minLength(5), Validators.maxLength(15)])],
+      lastName: ['', Validators.compose([Validators.required, Validators.minLength(5), Validators.maxLength(15)])],
+      email: ['', Validators.required],
+      password: ['', Validators.compose([Validators.required, Validators.minLength(4)])],
+      confirmedPassword: ['', Validators.compose([Validators.required, Validators.minLength(4)])]
     });
 
     this.options = { timeOut: 2500, pauseOnHover: true, showProgressBar: false, animate: 'scale', position: ['right', 'top'] };
   }
 
   register(): void {
-    console.log(this.userToRegister.value);
-    this._authService
-      .register(this.userToRegister.value)
-      .subscribe((res: any) => {
-        this._notification.success('', res.message);
-        setTimeout(() => this._router.navigateByUrl('/login'), 2500);
-      },
-      (err: any) => {
-        let notificationMsg = JSON.parse(err._body).message;
-        this._notification.error('', notificationMsg);
-      });
+    if (this.userToRegister.value.password !== this.userToRegister.value.confirmedPassword) {
+      this._notification.error('', 'Passwords do not match');
+    } else {
+      this._authService
+        .register(this.userToRegister.value)
+        .subscribe((res: any) => {
+          this._notification.success('', res.message);
+          setTimeout(() => this._router.navigateByUrl('/login'), 2500);
+        },
+        error => {
+          const errorRes = error.json();
+          if (errorRes.error) {
+            this._notification.error('', error.json().error.message);
+          } else {
+            this._notification.error('', 'Register unsuccessful');
+          }
+        });
+    }
   }
 }
