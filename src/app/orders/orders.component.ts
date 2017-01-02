f7f7349dfc51bdf0ad062e562c890ebd45d4668c
MartinYotov <m.yotov96@gmail.com>
2017-01-02 10:46:54 +0200

    Orders page done

--- src/app/orders/orders.component.ts
+++ src/app/orders/orders.component.ts
@@ -11,17 +11,44 @@ import { UserService } from '../../services/user.service';
 })
 export class OrdersComponent implements OnInit {
 
-  orders: Order[] = [];
+  pastOrders: Order[] = [];
+  activeOrders: Order[] = [];
   haveOrders: boolean;
+  havePastOrders: boolean;
+  haveActiveOrders: boolean;
 
   constructor(private UserService: UserService) { }
 
   ngOnInit() {
     this.UserService.getOrders()
       .subscribe(res => {
-        this.orders = res.result.orders;
-        if (this.orders.length > 0) {
+        this.pastOrders = res.result.orders;
+        let pastOrdersCount = this.pastOrders.length;
+        if (pastOrdersCount > 0) {
           this.haveOrders = true;
+
+          let i = pastOrdersCount - 1;
+          let orderDate;
+          let dateTime50MinsAgo = new Date();
+          dateTime50MinsAgo.setMinutes(dateTime50MinsAgo.getMinutes() - 50);
+
+          for(i; i >= 0; i -= 1) {
+            let orderDate = new Date(this.pastOrders[i].orderDate);
+            if(orderDate < dateTime50MinsAgo) {
+              i += 1;
+              break;
+            }
+          }
+          if(i === -1){ //all orders are active
+            this.activeOrders = this.pastOrders;
+            this.haveActiveOrders = true; 
+          }else if(i < pastOrdersCount) { //some orders are active and some are past
+            this.activeOrders = this.pastOrders.splice(i, pastOrdersCount - i);
+            this.haveActiveOrders = true;
+            this.havePastOrders = true;
+          }else{ //all orders are past
+            this.havePastOrders = true;
+          }
         }
       });
   }
