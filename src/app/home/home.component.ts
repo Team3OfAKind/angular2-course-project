55036fd4ba8f186680795dcb45bf7e15b5bc05b2
Silviya <sbboteva@gmail.com>
2017-01-02 20:42:03 +0200

    added new animation on home page, fixed order bug

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input,
+  trigger,
+  state,
+  style,
+  transition,
+  animate, 
+  keyframes } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { InfoService } from '../../services/info.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css', '../app.component.css']
+  styleUrls: ['./home.component.css', '../app.component.css'],
+  animations: [
+  trigger('flyInOut', [
+    state('in', style({transform: 'translateX(0)'})),
+    transition('void => *', [
+      animate(300, keyframes([
+        style({opacity: 0, transform: 'translateX(-100%)', offset: 0}),
+        style({opacity: 1, transform: 'translateX(15px)',  offset: 0.3}),
+        style({opacity: 1, transform: 'translateX(0)',     offset: 1.0})
+      ]))
+    ]),
+    transition('* => void', [
+      animate(500, keyframes([
+        style({opacity: 1, transform: 'translateX(0)',     offset: 0}),
+        style({opacity: 1, transform: 'translateX(-15px)', offset: 0.7}),
+        style({opacity: 0, transform: 'translateX(100%)',  offset: 1.0})
+      ]))
+    ])
+  ])
+]
 })
 export class HomeComponent implements OnInit {
   info: any;
