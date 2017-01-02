c87f62674148466ae640e7d5f16c5d07c1e4ce74
Silviya <sbboteva@gmail.com>
2017-01-02 10:56:29 +0200

    implemented loading component

--- src/app/animations/loading-animation/loading-animation.component.ts
+++ src/app/animations/loading-animation/loading-animation.component.ts
@@ -1,33 +1,9 @@
-import { Component, OnInit, trigger, state, style, transition, animate } from '@angular/core';
-import { LoadingAnimateService } from '../../../services/loading.service';
+import { Component } from '@angular/core';
 
 @Component({
-    selector: 'loading-animate',
-    template: './loading-animation.component.html',
-    styles: ['./loading-animation.component.css'],
-    animations: [
-        trigger('loadingState', [
-            state( 'inactive', style({
-                opacity: 0,
-                display: 'none'
-            })),
-            state('active',   style({
-                opacity: 1,
-            })),
-            transition('active => inactive', animate('200ms ease-out')),
-            transition('inactive => active', animate('0ms ease-in'))
-        ])
-    ]
+  selector: 'app-loader',
+  templateUrl: './loading-animation.component.html',
+  styleUrls: ['./loading-animation.component.css']
 })
-export class LoadingAnimateComponent implements OnInit {
-
-    loadingState: string = 'inactive';
-
-    constructor(private _loadingSvc: LoadingAnimateService) {}
-
-    ngOnInit(): void {
-        this._loadingSvc.getValue().subscribe( (status: boolean) => {
-            this.loadingState = status ? 'active' : 'inactive';
-        });
-    }
+export class LoadingAnimateComponent {
 }
